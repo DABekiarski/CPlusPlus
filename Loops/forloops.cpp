7c68bfc87543c71b24c0d91665f0219522b059d2
DABekiarski <dimitar.bekiarski@gmail.com>
2025-03-04 07:52:42 +0200

    Update forloops.cpp

--- Loops/forloops.cpp
+++ Loops/forloops.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int i;
-	char ch; 
-	
+	int i;	
 	for(i = 0; i < 100; i++){ // Endind on 7 
 		if(i %10 == 7){
 			cout<<i<<endl;
 		}
 	}
 	
-	cout<<"alphabet" <<endl; // Prints a,b,c,d,...,z
-	
-	for(ch = 'a'; i <= 'z'; ch++){
-	
-			cout<<ch<<endl;
-		
-	}
 	return 0;
 }
