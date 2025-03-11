a917ac865224d8736fb18f6d19deb7f8631b850f
DABekiarski <dimitar.bekiarski@gmail.com>
2025-03-11 07:33:25 +0200

    Add files via upload

---
+++ Arrays/DTArr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	double dbarr[5] = {3.14,678.232,4343.2323};
+	float flarr[5] = {23.14,435.232,7643.9};
+	char charray[6] = {'A','+','?','<'};
+		
+	cout<<"dbarr[0]: "<< dbarr[0] << endl;
+	cout<<"dbarr[1]: "<< dbarr[1]<<endl;
+	cout<<"dbarr[2]: "<<dbarr[2]<<endl;
+	cout<<"dbarr[3]: "<< dbarr[3]<<endl;
+	
+	cout<<"flarr[0]: "<< flarr[0] << endl;
+	cout<<"flarr[1]: "<< flarr[1]<<endl;
+	cout<<"flarr[2]: "<<flarr[2]<<endl;
+	cout<<"flarr[3]: "<< flarr[3]<<endl;
+	
+	cout<<"charray[0]: "<< charray[0] << endl;
+	cout<<"charray[1]: "<< charray[1]<<endl;
+	cout<<"charray[2]: "<<charray[2]<<endl;
+	cout<<"charray[3]: "<< charray[3]<<endl;
+	
+	return 0;
+}
